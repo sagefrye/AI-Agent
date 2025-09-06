1a7e62d4e5e9fc5df5f065f542cab0f2431e5cc5
Sage Frye <sagefrye013@gmail.com>
2025-09-06 10:47:22 -0500

    added verbose option

--- main.py
+++ main.py
@@ -2,25 +2,40 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
+
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    if not args:
+        print("No request was provided")
+        sys.exit(1)
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    if len(sys.argv) < 2:
-        print("No request was provided")
-        exit(1)
-    request = sys.argv[1]
+
+    request = " ".join(args)
+
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=request)]),
+    ]
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents=request
+        contents=messages
     )
-    if request:
-        print(response.text)
+
+    if verbose:
+        print(f"User prompt: {request}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    else:
-        exit(1)
+    print(f"Response: {response.text}")
 
 if __name__ == "__main__":
     main()
